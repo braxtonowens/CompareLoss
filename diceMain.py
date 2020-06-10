38892ff083e4bd1f826896431c5112796a716846
braxtonowens <braxton.owens.hhs@gmail.com>
2020-06-10 12:35:12 -0400

    first commit

--- diceMain.py
+++ diceMain.py
@@ -14,7 +14,7 @@ data_gen_args = dict(rotation_range=0.2,
 myGene = trainGenerator(2,'CompareLoss/data/membrane/train','image','label',data_gen_args,save_to_dir = None)
 
 model = unet()
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
+model_checkpoint = ModelCheckpoint('dice_unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
 model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
 
 testGene = testGenerator("CompareLoss/data/membrane/test")
